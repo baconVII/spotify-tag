c3ffb2e515c821ee522d9355b1d7417d577353d5
baconVII <baconvii485@gmail.com>
2014-11-03 22:14:13 +0100

    Connect to Spotify Account and load Library into the console

--- web/public/tags.js
+++ web/public/tags.js
@@ -1,8 +1,37 @@
 $(document).ready(
     function() {
-        $("#login").on('click', function() {
-            loginSpotify();
-        });
+        var stateKey = 'spotify_auth_state';
+        var spotifyApi = new SpotifyWebApi();
+
+        var args = getHashParams();
+
+        var access_token = args.access_token;
+        var state = args.state;
+        var storedState = localStorage.getItem(stateKey);
+
+        if (access_token && (state == null || state !== storedState)) {
+             alert('There was an error during the authentication');
+        } else {
+            $("#login").hide();
+
+            localStorage.removeItem(stateKey);
+
+            spotifyApi.setAccessToken(access_token);
+
+            if(access_token){
+                spotifyApi.getMySavedTracks()
+                    .then(function(data){
+                        console.log(data);
+                    }, function(err){
+                        console.error(err);
+                    });
+            } else{
+                $("#login").show();
+                $("#login").on('click', function() {
+                    loginSpotify();
+                });
+            }
+        }
     }
 );
 
@@ -24,15 +53,24 @@ function loginSpotify() {
     url += '&state=' + encodeURIComponent(state);
 
     window.location = url;
-};
+}
 
 function generateRandomString(length) {
-      var text = '';
-      var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-      for (var i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-      }
-      return text;
-};
+    for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
 
+function getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    while ( e = r.exec(q)) {
+     hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+}
